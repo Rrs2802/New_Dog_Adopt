2b111d6b2df62925751ce21dad573d15c65e9193
Rrs2802 <shettyritesh282@gmail.com>
2023-11-03 05:09:00 +0530

    first commit

---
+++ includes/sql/db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE db (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    email2 VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    password2 VARCHAR(255) NOT NULL
+);
